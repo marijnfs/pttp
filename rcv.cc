f2eb34a8a9dbcdb0fdac7379891ddec0f805acc4
Marijn Stollenga <m.stollenga@gmail.com>
2016-06-18 01:34:04 +0200

    request test case

--- rcv.cc
+++ rcv.cc
@@ -2,6 +2,8 @@
 #include <cassert>
 #include <vector>
 #include <iostream>
+#include <cstdint>
+
 #include "err.h"
 
 using namespace std;
@@ -12,10 +14,11 @@ int main() {
 
   void *sock = zmq_socket(ctx, ZMQ_REP);
 
-  if (zmq_connect(sock, "tcp://*:5555") == -1)
+  if (zmq_bind(sock, "tcp://127.0.0.1:1234") == -1)
     throw Err("couldnt connect");
 
   {
+    cout << "get request" << endl;
     zmq_msg_t *msg = new zmq_msg_t;
     
     int rc = zmq_msg_init (msg);
@@ -24,26 +27,27 @@ int main() {
     cout << "recved: " << rc << "bytes" << endl;
     assert (rc >= 0);
     
-    vector<char> bytes(rc);
+    vector<uint8_t> bytes(rc);
     copy((char*)zmq_msg_data(msg), (char*)zmq_msg_data(msg) + rc, &bytes[0]);
     for (size_t i(0); i < bytes.size(); ++i)
-      cout << bytes[i] << " ";
+      cout << (int)bytes[i] << " ";
     cout << endl;
   }
 
 
     
   {
+    cout << "send response" << endl;
     zmq_msg_t *msg = new zmq_msg_t;
     int rc = zmq_msg_init_size (msg, 10);
-    vector<char> bytes(10);
+    vector<uint8_t> bytes(10);
     for (size_t i(0); i < bytes.size(); ++i)
       bytes[i] = i;
     copy(&bytes[0], &bytes[bytes.size()], (char*)zmq_msg_data(msg));
     assert (rc == 0);
     
     rc = zmq_msg_send(msg, sock, 0);//ZMQ_DONTWAIT for polling
-    assert (rc == 0);
+    assert (rc != -1);
     //int nbytes = zmq_recv (socket, &msg, 0);
   }
 
--- snd.cc
+++ snd.cc
@@ -2,6 +2,9 @@
 #include <cassert>
 #include <vector>
 #include <iostream>
+#include <cstdint>
+
+#include "err.h"
 
 using namespace std;
 
@@ -12,34 +15,37 @@ int main() {
   
   void *sock = zmq_socket(ctx, ZMQ_REQ);
   if (!sock)
-    return 1;
+    throw Err("no socket");
   
-  if (zmq_connect(sock, "tcp://*:5555") == -1)
-    return 1;
+  if (zmq_connect(sock, "tcp://127.0.0.1:1234") == -1)
+    throw Err("couldnt connect");
 
   {
+    cout << "send request" << endl;
     zmq_msg_t *msg = new zmq_msg_t;
     int rc = zmq_msg_init_size (msg, 10);
-    vector<char> bytes(10);
+    vector<uint8_t> bytes(10);
     for (size_t i(0); i < bytes.size(); ++i)
       bytes[i] = i;
     copy(&bytes[0], &bytes[bytes.size()], (char*)zmq_msg_data(msg));
     assert (rc == 0);
     
     rc = zmq_msg_send(msg, sock, 0);//ZMQ_DONTWAIT for polling
-    assert (rc == 0);
+    assert (rc != -1);
+    cout << "sent " << rc << " bytes" << endl;
     //int nbytes = zmq_recv (socket, &msg, 0);
   }
 
   {
-      zmq_msg_t *msg = new zmq_msg_t;
+    cout << "rcv response" << endl;
+    zmq_msg_t *msg = new zmq_msg_t;
       
       int rc = zmq_msg_init (msg);
       
       rc = zmq_msg_recv(msg, sock, 0);//ZMQ_DONTWAIT for polling
       assert (rc >= 0);
       
-      vector<char> bytes(rc);
+      vector<uint8_t> bytes(rc);
       copy((char*)zmq_msg_data(msg), (char*)zmq_msg_data(msg) + rc, &bytes[0]);
       for (size_t i(0); i < bytes.size(); ++i)
 	cout << bytes[i] << " ";
